95e548ce599432045c169006a73f94921f892f23
Lynde Capogreco <lyndecapc@gmail.com>
2020-09-17 11:20:50 -0700

    CTD TEST Changes

--- ViewModels/CTDateConstraint.cs
+++ ViewModels/CTDateConstraint.cs
@@ -1,22 +1,40 @@
 ﻿using ESAPIX.Constraints;
+using ESAPIX.Extensions;
+using System;
 using VMS.TPS.Common.Model.API;
 
 namespace PlanChecker.ViewModels
 {
-    internal class CTDateConstraint : IConstraint
+    public class CTDateConstraint : IConstraint
     {
-        public string Name => throw new System.NotImplementedException();
-
-        public string FullName => throw new System.NotImplementedException();
+        public string Name => "CT Date Checker";
+        public string FullName => "CT Date < 60 Days";
 
         public ConstraintResult CanConstrain(PlanningItem pi)
         {
-            throw new System.NotImplementedException();
+            var pq = new PQAsserter(pi);
+            return pq.HasImage().CumulativeResult;
         }
 
         public ConstraintResult Constrain(PlanningItem pi)
         {
-            throw new System.NotImplementedException();
+            var image = pi.GetImage();
+            return Constrain(image.CreationDateTime);
+        }
+
+        public ConstraintResult Constrain(DateTime? creationDate)
+        {
+            var diffDays = (DateTime.Now - creationDate).Value.TotalDays;
+            var msg = $"CT is {diffDays} days old";
+
+            if (diffDays <= 60)
+            {
+                return new ConstraintResult(this, ResultType.PASSED, msg);
+            }
+            else
+            {
+                return new ConstraintResult(this, ResultType.ACTION_LEVEL_3, msg);
+            }
         }
     }
 }
