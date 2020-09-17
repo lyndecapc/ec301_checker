f50837ae55a5c607c379095a270550fefa80dda4
Lynde Capogreco <lyndecapc@gmail.com>
2020-09-17 10:35:12 -0700

    updTED ctd date constraints and mainviewmodel

---
+++ ViewModels/CTDateConstraint.cs
@@ -0,0 +1,22 @@
+﻿using ESAPIX.Constraints;
+using VMS.TPS.Common.Model.API;
+
+namespace PlanChecker.ViewModels
+{
+    internal class CTDateConstraint : IConstraint
+    {
+        public string Name => throw new System.NotImplementedException();
+
+        public string FullName => throw new System.NotImplementedException();
+
+        public ConstraintResult CanConstrain(PlanningItem pi)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ConstraintResult Constrain(PlanningItem pi)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
